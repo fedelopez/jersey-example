6fdc6151771bb85d203ae5e7d30635070b5f2c71
Fede Lopez <fede.lopez@gmail.com>
2012-05-18 13:06:47 +0200

    Added json with padding.

--- src/main/java/com/InterpretResource.java
+++ src/main/java/com/InterpretResource.java
@@ -9,22 +9,18 @@ import javax.ws.rs.Produces;
 /**
  * @author FedericoL
  */
-// The Java class will be hosted at the URI path "/helloworld"
-@Path("/interpret")
+@Path("/interpret") // The Java class will be hosted at the URI path "/interpret"
 public class InterpretResource {
-    // The Java method will process HTTP GET requests
+
     @GET
-    // The Java method will produce content identified by the MIME Media
-    // type "text/plain"
     @Produces({"application/json"})
     public String getInterpretation() {
         Gson gson = new Gson();
         Question question = new Question();
-        question.attribute = "at_least_weekly";
-        question.mandatory = true;
         question.text = "Do you drive more than once a week?";
-        gson.toJson(question);
-        return gson.toJson(question);
+        question.mandatory = true;
+        question.attribute = "at_least_weekly";
+        return "callback(" + gson.toJson(question) + ")";
     }
 
     private static class Question {
